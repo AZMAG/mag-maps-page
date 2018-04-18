cea65a7432fca2429c827f0000b4fbc57e95bace
Vern Wolfley <vwolfley@azmag.gov>
2018-04-18 09:14:26 -0700

    rename files modify css

--- src/js/main.js
+++ src/js/main.js
@@ -11,21 +11,42 @@
 $(document).ready(function() {
     "use strict";
     //*** Load page sections
-    $("#carousel-section").load("views/carousel-section.html");
-    $("#maps-section").load("views/maps-section.html");
-    $("#video-section").load("views/video-section.html");
-    $(".footer-section").load("views/footer-section.html", function() {
+
+    //Load nav bar then run function that adds active to nav link
+    $("#navbar-section").load("views/section-navbar.html", function() {
+        $("#magNavbar .nav li a").each(function() {
+            if ($(this).prop("href") === window.location.href) {
+                $(this).parent("li").addClass("active");
+            }
+        });
+
+        /*twitter share button href*/
+        var baseUrl = 'http://twitter.com/intent/tweet';
+        var text = "MAG%20%7C%20Mapping%20Center";
+        var thisPageUrl = "http://maps.azmag.gov/";
+        var hashTag = "MAGmaps";
+        var via = "MAGregion";
+
+        var twitterHref = (baseUrl + "?text=" + text + "&url=" + thisPageUrl + "&hashtag=" + hashTag + "&via=" + via);
+
+        $('#TWshareButton').attr('href', twitterHref);
+    });
+
+    $("#carousel-section").load("views/section-carousel.html");
+    $("#maps-section").load("views/section-maps.html");
+    $("#video-section").load("views/section-video.html");
+    $(".footer-section").load("views/section-footer.html", function() {
         //*** terms binding
-        $("#termsModal").load("views/terms.html");
+        $("#termsModal").load("views/modal-terms.html");
         //*** privacy binding
-        $("#privacyModal").load("views/privacy.html");
+        $("#privacyModal").load("views/modal-privacy.html");
         //*** version binding
         $(".version").html("v5.0.0 | 2018-04-12");
         //*** copy write binding
         $(".copyright").html("2018");
     });
-    $("#pastUpdates").load("views/pastUpdates.html");
-    $("#mostRecent").load("views/recentUpdates.html");
+    $("#pastUpdates").load("views/section-pastUpdates.html");
+    $("#mostRecent").load("views/section-recentUpdates.html");
 
     //*** Modals ***
     $("#stateDemographics").load("views/modal-stateDemographics.html");
@@ -47,7 +68,7 @@ $(document).ready(function() {
  ***********************************
  */
 
-// twitter
+// twitter widget js loading
 ! function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
         p = /^http:/.test(d.location) ? "http" : "https";
@@ -59,6 +80,11 @@ $(document).ready(function() {
     }
 }(document, "script", "twitter-wjs");
 
+//LinkedIn
+$(document).on('click', '#INshareButton', function() {
+    window.open("https://www.linkedin.com/shareArticle?url=http://maps.azmag.gov/", "shareLinkedIn", 'width=600, height=600');
+});
+
 // google +1
 (function() {
     var po = document.createElement("script");
@@ -69,7 +95,20 @@ $(document).ready(function() {
     s.parentNode.insertBefore(po, s);
 })();
 
+$(document).on('click', '#GPlusShareButton', function() {
+    window.open("//plus.google.com/share?url=http%3A%2F%2Fmaps.azmag.gov%2F", 'shareGooglePlus', 'width=400, height=700');
+});
+
 // facebook
+//share dialog - use on('click') for it to work afte loading html
+$(document).on('click', '#FBshareButton', function() {
+    FB.ui({
+        method: 'share',
+        mobile_iframe: true,
+        href: 'https://developers.facebook.com/docs/',
+    }, function(response) {});
+});
+
 window.fbAsyncInit = function() {
     FB.init({
         appId: "929950963769905",
@@ -88,8 +127,34 @@ window.fbAsyncInit = function() {
 }(document, "script", "facebook-jssdk"));
 
 
-//*********** NEW ******
+$(document).on('click', '.shareButton button', function(e) {
 
+    e.preventDefault();
+    $(this).toggleClass('active');
+    var shareBox = $(this).attr('data-target');
+
+    if ($(this).hasClass('active')) {
+        $(shareBox).show();
+    }
+});
+
+$('body').mouseup(function(e) {
+    var parentTarget = $(e.target).parents('#shareBox');
+
+    if (e.target.id !== 'shareBox' && parentTarget.length < 1 && $(e.target).hasClass('shareButton') == false) {
+        $('#shareBox').hide();
+        $('.shareButton button').removeClass('active');
+    }
+});
+
+//Open email window
+function openNewWindow(obj) {
+    "use strict";
+    var thisHref = obj.href;
+    window.open(thisHref, 'popup', 'width=600,height=600');
+}
+
+//collapse one at a time
 $(".toggle-links").click(function() {
     "use strict";
     $(".collapseGroup .collapse").collapse("hide");
@@ -102,17 +167,6 @@ $(".toggle-links").click(function() {
         $(this).addClass("active");
     }
 });
-
-//Change active on navigation
-$(function() {
-    "use strict";
-    $("#magNavbar .nav li a").each(function() {
-        if ($(this).prop("href") === window.location.href) {
-            $(this).parent("li").addClass("active");
-        }
-    });
-});
-
 
 $(window).scroll(function() {
     "use strict";

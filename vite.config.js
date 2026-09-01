e8a1027eb3c2f2199a620a5dee6a4959bc3386c6
Vern Wolfley <vwolfley@azmag.gov>
2026-09-01 14:26:48 -0700

    add jsconfig file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import dns from "dns";
+import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 dns.setDefaultResultOrder("verbatim");
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: "./",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     server: {
         host: "localhost",
         port: 3000,
